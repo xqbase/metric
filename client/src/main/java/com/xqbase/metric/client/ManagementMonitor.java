74e8297bb63db910c8fb5660b30983c9bc1617fc
auntyellow <webmaster@xqbase.com>
2014-10-08 13:20:56 +0800

    ignore .settings folder in Eclipse

--- client/src/main/java/com/xqbase/metric/client/ManagementMonitor.java
+++ client/src/main/java/com/xqbase/metric/client/ManagementMonitor.java
@@ -9,11 +9,11 @@ import com.sun.management.OperatingSystemMXBean;
 import com.xqbase.metric.common.Metric;
 
 public class ManagementMonitor implements Runnable {
-	private static double inMB(long value) {
+	private static double MB(long value) {
 		return (double) value / 1048576;
 	}
 
-	private static double inPercent(long dividend, long divisor) {
+	private static double PERCENT(long dividend, long divisor) {
 		return divisor == 0 ? 0 : (double) dividend * 100 / divisor; 
 	}
 
@@ -36,13 +36,13 @@ public class ManagementMonitor implements Runnable {
 		// Metric.add(memory, inMB(rt.totalMemory() - rt.freeMemory()), "type", "heap_used");
 		MemoryMXBean memory_ = ManagementFactory.getMemoryMXBean();
 		MemoryUsage heap = memory_.getHeapMemoryUsage();
-		Metric.put(memoryMB, inMB(heap.getCommitted()), "type", "heap_committed");
-		Metric.put(memoryMB, inMB(heap.getUsed()), "type", "heap_used");
-		Metric.put(memoryPercent, inPercent(heap.getUsed(), heap.getMax()), "type", "heap");
+		Metric.put(memoryMB, MB(heap.getCommitted()), "type", "heap_committed");
+		Metric.put(memoryMB, MB(heap.getUsed()), "type", "heap_used");
+		Metric.put(memoryPercent, PERCENT(heap.getUsed(), heap.getMax()), "type", "heap");
 		MemoryUsage nonHeap = memory_.getNonHeapMemoryUsage();
-		Metric.put(memoryMB, inMB(nonHeap.getCommitted()), "type", "non_heap_committed");
-		Metric.put(memoryMB, inMB(nonHeap.getUsed()), "type", "non_heap_used");
-		Metric.put(memoryPercent, inPercent(nonHeap.getUsed(), nonHeap.getMax()), "type", "non_heap");
+		Metric.put(memoryMB, MB(nonHeap.getCommitted()), "type", "non_heap_committed");
+		Metric.put(memoryMB, MB(nonHeap.getUsed()), "type", "non_heap_used");
+		Metric.put(memoryPercent, PERCENT(nonHeap.getUsed(), nonHeap.getMax()), "type", "non_heap");
 
 		java.lang.management.OperatingSystemMXBean os_ =
 				ManagementFactory.getOperatingSystemMXBean();
@@ -52,15 +52,15 @@ public class ManagementMonitor implements Runnable {
 		OperatingSystemMXBean os = (OperatingSystemMXBean) os_;
 		long totalPhysical = os.getTotalPhysicalMemorySize();
 		long usedPhysical = totalPhysical - os.getFreePhysicalMemorySize();
-		Metric.put(memoryMB, inMB(usedPhysical), "type", "physical");
-		Metric.put(memoryPercent, inPercent(usedPhysical, totalPhysical),
+		Metric.put(memoryMB, MB(usedPhysical), "type", "physical");
+		Metric.put(memoryPercent, PERCENT(usedPhysical, totalPhysical),
 				"type", "physical_memory");
 		long totalSwap = os.getTotalSwapSpaceSize();
 		long usedSwap = totalSwap - os.getFreeSwapSpaceSize();
-		Metric.put(memoryMB, inMB(usedSwap), "type", "swap");
-		Metric.put(memoryMB, inMB(os.getCommittedVirtualMemorySize()),
+		Metric.put(memoryMB, MB(usedSwap), "type", "swap");
+		Metric.put(memoryMB, MB(os.getCommittedVirtualMemorySize()),
 				"type", "process_committed");
-		Metric.put(memoryPercent, inPercent(usedSwap, totalSwap),
+		Metric.put(memoryPercent, PERCENT(usedSwap, totalSwap),
 				"type", "swap_space");
 
 		double system = os.getSystemCpuLoad();
