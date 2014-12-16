51ae9e6f357c91e8aaf63e8d56d392ab8c7e23c5
auntyellow <webmaster@xqbase.com>
2014-12-16 21:43:09 +0800

    quarterly aggregation

--- collector/src/main/java/com/xqbase/metric/collector/Collector.java
+++ collector/src/main/java/com/xqbase/metric/collector/Collector.java
@@ -28,6 +28,7 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.xqbase.metric.common.Metric;
 import com.xqbase.metric.common.MetricEntry;
+import com.xqbase.metric.common.MetricValue;
 import com.xqbase.util.ByteArrayQueue;
 import com.xqbase.util.Conf;
 import com.xqbase.util.Log;
@@ -38,6 +39,8 @@ import com.xqbase.util.Time;
 
 public class Collector {
 	private static final int MAX_BUFFER_SIZE = 64000;
+	private static final int QUARTER = 15;
+	private static final int DAY = 1440;
 
 	private static String decode(String s) {
 		try {
@@ -47,10 +50,10 @@ public class Collector {
 		}
 	}
 
-	private static BasicDBObject row(Map<String, String> tagMap,
+	private static BasicDBObject row(Map<String, String> tagMap, String type,
 			int now, int count, double sum, double max, double min, double sqr) {
 		BasicDBObject row = new BasicDBObject(tagMap);
-		row.put("_minute", Integer.valueOf(now));
+		row.put(type, Integer.valueOf(now));
 		row.put("_count", Integer.valueOf(count));
 		row.put("_sum", Double.valueOf(sum));
 		row.put("_max", Double.valueOf(max));
@@ -81,10 +84,187 @@ public class Collector {
 		}
 	}
 
-	private static final BasicDBObject INDEX_KEY = new BasicDBObject("_minute", Integer.valueOf(1));
+	private static boolean isTag(String key) {
+		return !key.isEmpty() && key.charAt(0) != '_';
+	}
+
+	private static int getInt(DBObject row, String key) {
+		Object value = row.get(key);
+		return value instanceof Number ? ((Number) value).intValue() : 0;
+	}
+
+	private static double getDouble(DBObject row, String key) {
+		Object value = row.get(key);
+		return value instanceof Number ? ((Number) value).doubleValue() : 0;
+	}
+
+	private static String getString(DBObject row, String key) {
+		Object value = row.get(key);
+		return value instanceof String ? (String) value : "_";
+	}
+
+	private static BasicDBObject __(String key, Object value) {
+		return new BasicDBObject(key, value);
+	}
+
+	private static final BasicDBObject INDEX_MINUTE = __("_minute", Integer.valueOf(1));
+	private static final BasicDBObject INDEX_QUARTER = __("_quarter", Integer.valueOf(1));
+	private static final BasicDBObject INDEX_NAME = __("_name", Integer.valueOf(1));
 
 	private static ShutdownHook hook = new ShutdownHook();
+	private static AtomicInteger minuteNow = new AtomicInteger();
+	private static AtomicInteger quarterNow = new AtomicInteger();
+	private static int serverId, expire;
 	private static boolean verbose;
+
+	private static void minutely(DB db) {
+		int now = minuteNow.getAndIncrement();
+		// Insert aggregation-during-collection metrics
+		HashMap<String, ArrayList<DBObject>> rowsMap = new HashMap<>();
+		for (MetricEntry entry : Metric.removeAll()) {
+			BasicDBObject row = row(entry.getTagMap(), "_minute", now, entry.getCount(),
+					entry.getSum(), entry.getMax(), entry.getMin(), entry.getSqr());
+			put(rowsMap, entry.getName(), row);
+		}
+		if (!rowsMap.isEmpty()) {
+			insert(db, rowsMap);
+		}
+		// Ensure index and calculate metric size by master collector
+		if (serverId != 0) {
+			return;
+		}
+		ArrayList<DBObject> rows = new ArrayList<>();
+		for (String name : db.getCollectionNames()) {
+			if (name.startsWith("system.") || name.equals("_tags")) {
+				continue;
+			}
+			DBCollection collection = db.getCollection(name);
+			if (!name.startsWith("_quarter.")) {
+				collection.createIndex(INDEX_MINUTE);
+			}
+			int count = (int) collection.count();
+			rows.add(row(Collections.singletonMap("name", name), "_minute",
+					now, 1, count, count, count, count * count));
+		}
+		if (!rows.isEmpty()) {
+			db.getCollection("metric.size").insert(rows);
+		}
+	}
+
+	private static void quarterly(DB db) {
+		int now = quarterNow.getAndIncrement();
+		BasicDBObject removeBefore = __("_minute", __("$lt",
+				Integer.valueOf(now * QUARTER - expire)));
+		BasicDBObject removeAfter = __("_minute", __("$gt",
+				Integer.valueOf(now * QUARTER + expire)));
+		BasicDBObject removeBeforeQuarter = __("_quarter", __("$lt",
+				Integer.valueOf(now - expire)));
+		BasicDBObject removeAfterQuarter = __("_quarter", __("$gt",
+				Integer.valueOf(now + expire)));
+		// Ensure index on tags
+		DBCollection tags = db.getCollection("_tags");
+		tags.createIndex(INDEX_NAME);
+		// Scan minutely collections
+		for (String name : db.getCollectionNames()) {
+			if (name.startsWith("system.") || name.equals("_tags") ||
+					name.startsWith("_quarter.")) {
+				continue;
+			}
+			DBCollection collection = db.getCollection(name);
+			// Remove stale
+			collection.remove(removeBefore);
+			collection.remove(removeAfter);
+			// Aggregate to quarter
+			ArrayList<DBObject> rows = new ArrayList<>();
+			int begin = now - expire;
+			BasicDBObject tagsQuery = __("_name", name);
+			DBObject tagsRow = tags.findOne(tagsQuery);
+			begin = tagsRow == null ? 0 : getInt(tagsRow, "_quarter");
+			for (int i = (begin == 0 ? now - expire : begin) + 1; i <= now; i ++) {
+				HashMap<HashMap<String, String>, MetricValue> result = new HashMap<>();
+				BasicDBObject range = __("$gte", Integer.valueOf((i - 1) * QUARTER + 1));
+				range.put("$lte", Integer.valueOf(i * QUARTER));
+				for (DBObject row : collection.find(__("_minute", range))) {
+					HashMap<String, String> tag = new HashMap<>();
+					for (String tagKey : row.keySet()) {
+						if (isTag(tagKey)) {
+							tag.put(tagKey, getString(row, tagKey));
+						}
+					}
+					MetricValue newValue = new MetricValue(getInt(row, "_count"),
+							getDouble(row, "_sum"), getDouble(row, "_max"),
+							getDouble(row, "_min"), getDouble(row, "_sqr"));
+					MetricValue value = result.get(tag);
+					if (value == null) {
+						result.put(tag, newValue);
+					} else {
+						value.add(newValue);
+					}
+				}
+				for (Map.Entry<HashMap<String, String>, MetricValue> entry :
+						result.entrySet()) {
+					MetricValue value = entry.getValue();
+					rows.add(row(entry.getKey(), "_quarter", i,
+							value.getCount(), value.getSum(),
+							value.getMax(), value.getMin(), value.getSqr()));
+				}
+			}
+			BasicDBObject update = __("$set", __("_quarter", Integer.valueOf(now)));
+			tags.update(tagsQuery, update, true, false);
+			if (!rows.isEmpty()) {
+				db.getCollection("_quarter." + name).insert(rows);
+			}
+		}
+		// Scan quarterly collections
+		for (String name : db.getCollectionNames()) {
+			if (!name.startsWith("_quarter.")) {
+				continue;
+			}
+			DBCollection collection = db.getCollection(name);
+			// Ensure index
+			collection.createIndex(INDEX_QUARTER);
+			// Remove stale
+			collection.remove(removeBeforeQuarter);
+			collection.remove(removeAfterQuarter);
+			// Arrange tags
+			String minuteName = name.substring(9);
+			BasicDBObject unsetTags = new BasicDBObject();
+			BasicDBObject tagsQuery = __("_name", minuteName);
+			DBObject tagsRow = tags.findOne(tagsQuery);
+			if (tagsRow != null) {
+				for (String tagKey : tagsRow.keySet()) {
+					if (isTag(tagKey)) {
+						unsetTags.put(tagKey, Integer.valueOf(1));
+					}
+				}
+			}
+			HashMap<String, HashSet<String>> tagMap = new HashMap<>();
+			BasicDBObject range = __("$gte",
+					Integer.valueOf(now - DAY / QUARTER + 1));
+			range.put("$lte", Integer.valueOf(now)); // last 24 hours
+			for (DBObject row : db.getCollection(name).
+					find(__("_quarter", range))) {
+				for (String tagKey : row.keySet()) {
+					if (!isTag(tagKey)) {
+						continue;
+					}
+					HashSet<String> tagValues = tagMap.get(tagKey);
+					if (tagValues == null) {
+						tagValues = new HashSet<>();
+						tagMap.put(tagKey, tagValues);
+					}
+					tagValues.add(getString(row, tagKey));
+				}
+			}
+			unsetTags.keySet().removeAll(tagMap.keySet());
+			BasicDBObject update = __("$set", new BasicDBObject(tagMap));
+			tags.update(tagsQuery, update, true, false);
+			if (!unsetTags.isEmpty()) {
+				update = __("$unset", unsetTags);
+				tags.update(tagsQuery, update, true, false);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		if (hook.isShutdown(args)) {
@@ -99,8 +279,8 @@ public class Collector {
 		String host = p.getProperty("host");
 		host = host == null || host.isEmpty() ? "0.0.0.0" : host;
 		int port = Numbers.parseInt(p.getProperty("port"), 5514);
-		int serverId = Numbers.parseInt(p.getProperty("server_id"), 0);
-		int expire = Numbers.parseInt(p.getProperty("expire"), 43200);
+		serverId = Numbers.parseInt(p.getProperty("server_id"), 0);
+		expire = Numbers.parseInt(p.getProperty("expire"), 2880);
 		boolean enableRemoteAddr = Conf.getBoolean(p.getProperty("remote_addr"), true);
 		String allowedRemote = p.getProperty("allowed_remote");
 		HashSet<String> allowedRemotes = null;
@@ -109,65 +289,24 @@ public class Collector {
 		}
 		verbose = Conf.getBoolean(p.getProperty("verbose"), false);
 		long start = System.currentTimeMillis();
-		AtomicInteger now = new AtomicInteger((int) (start / Time.MINUTE));
+		minuteNow.set((int) (start / Time.MINUTE));
+		quarterNow.set((int) (start / Time.MINUTE / QUARTER));
 		p = Conf.load("Mongo");
 		MongoClient mongo = null;
-		Runnable schedule = null;
+		Runnable minutely = null;
 		try (DatagramSocket socket = new DatagramSocket(new
 				InetSocketAddress(host, port))) {
 			mongo = new MongoClient(p.getProperty("host"),
 					Numbers.parseInt(p.getProperty("port"), 27017));
 			DB db = mongo.getDB(p.getProperty("db"));
-			// Schedule Statistics and Index
-			schedule = () -> {
-				int now_ = now.getAndIncrement();
-				// Insert aggregation-during-collection metrics
-				HashMap<String, ArrayList<DBObject>> rowsMap = new HashMap<>();
-				for (MetricEntry entry : Metric.removeAll()) {
-					BasicDBObject row = row(entry.getTagMap(), now_, entry.getCount(),
-							entry.getSum(), entry.getMax(), entry.getMin(), entry.getSqr());
-					put(rowsMap, entry.getName(), row);
-				}
-				if (!rowsMap.isEmpty()) {
-					insert(db, rowsMap);
-				}
-				// Ensure index and calculate metric size by master collector
-				if (serverId == 0) {
-					ArrayList<DBObject> rows = new ArrayList<>();
-					for (String name : db.getCollectionNames()) {
-						if (name.startsWith("system.")) {
-							continue;
-						}
-						DBCollection collection = db.getCollection(name);
-						collection.createIndex(INDEX_KEY);
-						int count = (int) collection.count();
-						rows.add(row(Collections.singletonMap("name", name),
-								now_, 1, count, count, count, count * count));
-					}
-					if (!rows.isEmpty()) {
-						db.getCollection("metric.size").insert(rows);
-					}
-				}
-			};
-			timer.scheduleAtFixedRate(Runnables.wrap(schedule),
-					Time.MINUTE - start % Time.MINUTE, Time.MINUTE, TimeUnit.MILLISECONDS);
-			// Schedule removal of stale metrics by master collector
+			minutely = Runnables.wrap(() -> minutely(db));
+			timer.scheduleAtFixedRate(minutely, Time.MINUTE - start % Time.MINUTE,
+					Time.MINUTE, TimeUnit.MILLISECONDS);
 			if (serverId == 0) {
-				timer.scheduleAtFixedRate(Runnables.wrap(() -> {
-					Integer notBefore = Integer.valueOf(now.get() - expire);
-					Integer notAfter = Integer.valueOf(now.get() + expire);
-					for (String name : db.getCollectionNames()) {
-						if (name.startsWith("system.")) {
-							continue;
-						}
-						DBCollection collection = db.getCollection(name);
-						collection.remove(new BasicDBObject("_minute",
-								new BasicDBObject("$lt", notBefore)));
-						collection.remove(new BasicDBObject("_minute",
-								new BasicDBObject("$gt", notAfter)));
-					}
-				}), Time.HOUR - start % Time.HOUR + Time.MINUTE / 2,
-						Time.HOUR, TimeUnit.MILLISECONDS);
+				long quarter = Time.MINUTE * QUARTER;
+				timer.scheduleAtFixedRate(Runnables.wrap(() -> quarterly(db)),
+						quarter - (start - quarter / 12) % quarter,
+						quarter, TimeUnit.MILLISECONDS);
 			}
 
 			hook.register(socket);
@@ -256,7 +395,7 @@ public class Collector {
 						// For aggregation-before-collection metric, insert immediately
 						int count = Numbers.parseInt(paths[2]);
 						double sum = Numbers.parseDouble(paths[3]);
-						put(rowsMap, name, row(tagMap, Numbers.parseInt(paths[1], now.get()),
+						put(rowsMap, name, row(tagMap, "_minute", Numbers.parseInt(paths[1], minuteNow.get()),
 								count, sum, Numbers.parseDouble(paths[4]), Numbers.parseDouble(paths[5]),
 								paths.length == 6 ? sum * sum / count : Numbers.parseDouble(paths[6])));
 					}
@@ -272,8 +411,8 @@ public class Collector {
 			Log.e(e);
 		} finally {
 			// Do not do Mongo operations in main thread (may be interrupted)
-			if (schedule != null) {
-				executor.execute(Runnables.wrap(schedule));
+			if (minutely != null) {
+				executor.execute(minutely);
 			}
 			Runnables.shutdown(executor);
 			Runnables.shutdown(timer);
