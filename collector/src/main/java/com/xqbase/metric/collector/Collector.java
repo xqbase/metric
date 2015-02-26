3e1a7b3be4c72c78ec285d9dab698ec67af4fc25
auntyellow <webmaster@xqbase.com>
2015-02-26 12:04:53 +0800

    service script bugfix

--- collector/src/main/java/com/xqbase/metric/collector/Collector.java
+++ collector/src/main/java/com/xqbase/metric/collector/Collector.java
@@ -330,6 +330,8 @@ public class Collector {
 		if (!service.startup(args)) {
 			return;
 		}
+		System.setProperty("java.util.logging.SimpleFormatter.format",
+				"%1$tY-%1$tm-%1$td %1$tk:%1$tM:%1$tS.%1$tL %2$s%n%4$s: %5$s%6$s%n");
 		Logger logger = Log.getAndSet(Conf.openLogger("Collector.", 16777216, 10));
 		ExecutorService executor = Executors.newCachedThreadPool();
 		ScheduledThreadPoolExecutor timer = new ScheduledThreadPoolExecutor(1);
