4de6e8876ab223a5b960bc4ac14e08be760bf1dc
auntyellow <webmaster@xqbase.com>
2016-06-08 15:51:27 +0800

    MetricValue Serialization Bugfix

--- common/src/main/java/com/xqbase/metric/common/MetricValue.java
+++ common/src/main/java/com/xqbase/metric/common/MetricValue.java
@@ -4,6 +4,10 @@ public class MetricValue implements Cloneable {
 	private long count;
 	private double sum, max, min, sqr;
 
+	protected MetricValue() {
+		// For Serialization
+	}
+
 	public MetricValue(long count, double sum, double max, double min, double sqr) {
 		this.count = count;
 		this.sum = sum;
